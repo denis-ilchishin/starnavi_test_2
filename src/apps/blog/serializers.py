ada973ea294d2ec77669cedc597bdec69cf8f71c
Denis Ilchishin <ilchishin.denis@gmail.com>
2020-11-10 13:08:10 +0000

    Add post analytics query serializer

--- src/apps/blog/serializers.py
+++ src/apps/blog/serializers.py
@@ -1,4 +1,6 @@
+from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from .models import Post
 
@@ -9,3 +11,16 @@ class PostCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ("user", "text")
+
+
+class PostAnalyticsQuerySerializers(serializers.Serializer):
+    date_from = serializers.DateField()
+    date_to = serializers.DateField()
+
+    def validate(self, attrs):
+        if attrs.get("date_from") >= attrs.get("date_to"):
+            raise ValidationError(
+                {"date_from": [_("`date_from` value must be lower that `date_to`")]}
+            )
+
+        return attrs
