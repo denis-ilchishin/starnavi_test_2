17844e8a0b372787a6c8cdee1b45d6ca3a2fe352
Denis Ilchishin <ilchishin.denis@gmail.com>
2020-11-10 10:47:48 +0000

    Add user activity view

--- src/apps/account/views.py
+++ src/apps/account/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class UserActivityView(APIView):
+    def get(self, request: Request):
+        return Response(
+            {
+                "last_login": request.user.last_login,
+                "last_activity": request.user.last_activity,
+            }
+        )
