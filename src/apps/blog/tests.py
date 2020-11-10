227aca7513a5c6e3445d610edcbbac619230bcc3
Denis Ilchishin <ilchishin.denis@gmail.com>
2020-11-10 05:12:31 +0000

    Add Post's rating tests

--- src/apps/blog/tests.py
+++ src/apps/blog/tests.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from .models import Post
+from .models import Post, PostLike
 
 
 class PostTests(APITestCase):
@@ -22,3 +22,40 @@ class PostTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Post.objects.get().text, data["text"])
         self.assertEqual(Post.objects.get().user, self.user)
+
+
+class PostsLikeTests(APITestCase):
+    """
+    In the real world we probably don't want to give users access to like theirs posts.
+    But right now for simplicity we don't mind
+    """
+
+    def setUp(self) -> None:
+        self.user = get_user_model().objects.create_user(
+            username="some-user", password="some-password"
+        )
+        self.post = Post.objects.create(user=self.user, text="some post's text")
+
+    def test_post_like(self):
+        self.client.force_authenticate(user=self.user)
+
+        response = self.client.post(
+            reverse("blog:posts:like", kwargs={"pk": self.post.pk})
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(PostLike.objects.count(), 1)
+        self.assertEqual(PostLike.objects.get().user, self.user)
+        self.assertEqual(PostLike.objects.get().post, self.post)
+
+    def test_post_unlike(self):
+        self.client.force_authenticate(user=self.user)
+
+        PostLike.objects.create(user=self.user, post=self.post)
+
+        response = self.client.post(
+            reverse("blog:posts:unlike", kwargs={"pk": self.post.pk})
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(PostLike.objects.count(), 0)
