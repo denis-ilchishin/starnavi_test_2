d77177197483dccac2c018d35861167c537a7176
Denis Ilchishin <ilchishin.denis@gmail.com>
2020-11-10 10:32:44 +0000

    Add account tests

--- src/apps/account/tests.py
+++ src/apps/account/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.utils import timezone
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class AuthenticationTests(APITestCase):
+    def setUp(self) -> None:
+        self.username = "some-username"
+        self.password = "some-password"
+
+        self.user = get_user_model().objects.create_user(
+            username=self.username, password=self.password
+        )
+
+    def test_user_last_login(self):
+        data = {"username": self.username, "password": self.password}
+
+        self.assertEqual(self.user.last_login, None)
+
+        response = self.client.post(reverse("authentication:signin"), data)
+
+        self.user.refresh_from_db()
+
+        self.assertAlmostEqual(
+            self.user.last_login.timestamp(), timezone.now().timestamp(), places=1
+        )
+
+    def test_user_last_activity(self):
+        self.client.force_authenticate(user=self.user)
+
+        response = self.client.post(
+            reverse("blog:posts:create"), {"text": "some post text here"}
+        )
+
+        self.user.refresh_from_db()
+
+        self.assertAlmostEqual(
+            self.user.last_activity.timestamp(), timezone.now().timestamp(), places=1
+        )
