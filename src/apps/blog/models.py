7f291347c1c790f82e38bd41139a823360c1c86e
Denis Ilchishin <ilchishin.denis@gmail.com>
2020-11-10 05:18:51 +0000

    Add PostLike model

--- src/apps/blog/models.py
+++ src/apps/blog/models.py
@@ -1,7 +1,25 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils import timezone
 
 
 class Post(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     text = models.CharField(max_length=255)
+
+
+class PostLike(models.Model):
+    post = models.ForeignKey(
+        "Post",
+        on_delete=models.CASCADE,
+        related_name="likes",
+        related_query_name="like",
+    )
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="post_likes",
+        related_query_name="post_like",
+    )
+
+    date_created = models.DateTimeField(default=timezone.now)
