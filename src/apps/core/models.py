df64cfd8e76cc29b8ebfbd4790af4bcdc5b3f6e7
Denis Ilchishin <ilchishin.denis@gmail.com>
2020-11-10 10:46:47 +0000

    Add `last_activity` attribute to user

--- src/apps/core/models.py
+++ src/apps/core/models.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 
 class User(AbstractUser):
-    pass
+    last_activity = models.DateTimeField(null=True)
+
+    def update_last_activity(self):
+        self.last_activity = timezone.now()
+        self.save()
