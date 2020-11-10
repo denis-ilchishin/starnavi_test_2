40b95304dca90d162643ef532e48773524844fc0
Denis Ilchishin <ilchishin.denis@gmail.com>
2020-11-10 04:56:23 +0000

    Add blog post creation tests

--- src/apps/blog/tests.py
+++ src/apps/blog/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from .models import Post
+
+
+class PostTests(APITestCase):
+    def setUp(self) -> None:
+        self.user = get_user_model().objects.create_user(
+            username="some-user", password="some-password"
+        )
+
+    def test_post_creation(self):
+        self.client.force_authenticate(user=self.user)
+
+        data = {"text": "some post text here"}
+
+        response = self.client.post(reverse("blog:posts:create"), data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Post.objects.get().text, data["text"])
+        self.assertEqual(Post.objects.get().user, self.user)
