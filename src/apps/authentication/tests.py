974e13a47c9c26501fb320d7ee9a92796798bf83
Denis Ilchishin <ilchishin.denis@gmail.com>
2020-11-10 02:46:30 +0000

    Add authentication app tests

--- src/apps/authentication/tests.py
+++ src/apps/authentication/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class AuthenticationTests(APITestCase):
+    def test_signup(self):
+        username = "some-username-example"
+        email = "some-mail@example.com"
+        password = password_confirm = "some-password-example"
+
+        data = {
+            "username": username,
+            "email": email,
+            "password": password,
+            "password_confirm": password_confirm,
+        }
+
+        response = self.client.post(reverse("authentication:signup", data))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(get_user_model().objects.count(), 1)
+        self.assertEqual(get_user_model().objects.get().username, username)
+        self.assertEqual(get_user_model().objects.get().email, email)
+
+    def test_signin(self):
+        username = "some-username-example"
+        password = "some-password-example"
+
+        user = get_user_model().objects.create_user(
+            username=username, password=password
+        )
+
+        data = {"username": username, "password": password}
+
+        response = self.client.post(reverse("authentication:signin", data))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(response.data, "access")
+        self.assertIn(response.data, "refresh")
